2cdf26526cb7e9e3d490e86839aff4aebfccbd7d
coulterminer <coultercash@gmail.com>
2023-06-01 14:06:38 -0600

    setup boot protocol

--- Kernel.cpp
+++ Kernel.cpp
@@ -1,3 +1,17 @@
-class Kernel {}
+void printf(char* str)
+{
+    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
+    
+    for(int i=0; str[i]!='\0';++i){
+        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    }  
+}
 
-int kernel_main(){}
+int kernel_boot(){
+	printf("COS 1.0\n");
+}
+
+extern "C" void kernel_main(void* multiboot_structure, uint32_t magicnumber)
+{
+	kernel_boot();
+}
